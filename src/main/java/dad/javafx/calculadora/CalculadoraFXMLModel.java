b99d58bda1eea0f5a43095858019e25251c195a3
AntonioRodriguezLuis <antoniorodriguezluis99@gmail.com>
2020-11-11 20:14:38 +0000

    Initial commit

---
+++ src/main/java/dad/javafx/calculadora/CalculadoraFXMLModel.java
@@ -0,0 +1,25 @@
+package dad.javafx.calculadora;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class CalculadoraFXMLModel {
+	
+	private StringProperty operacionesText = new SimpleStringProperty();
+
+	public final StringProperty operacionesTextProperty() {
+		return this.operacionesText;
+	}
+	
+
+	public final String getOperacionesText() {
+		return this.operacionesTextProperty().get();
+	}
+	
+
+	public final void setOperacionesText(final String operacionesText) {
+		this.operacionesTextProperty().set(operacionesText);
+	}
+	
+	
+}
